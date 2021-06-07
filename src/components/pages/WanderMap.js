01c3424f070d9bc5e8cc02375489d69bca2f233d
LouisDvr <louis.develter@gmail.com>
2021-06-07 12:07:37 +0200

    Add map view to app and react-native-maps dependencies

--- src/components/pages/WanderMap.js
+++ src/components/pages/WanderMap.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+import {StyleSheet, Text, View, TouchableHighlight, Dimensions} from "react-native";
+import MapView, {PROVIDER_GOOGLE} from "react-native-maps";
 
 export default function WanderMap() {
-    return (<View>
-        <Text>Map</Text>
-    </View>)
+    return (
+        <MapView style={styles.map}
+                 provider={PROVIDER_GOOGLE}
+                 showsUserLocation
+                 initialRegion={{
+                     latitude: 48.5112,
+                     longitude: 2.2055,
+                     latitudeDelta: 0.0922,
+                     longitudeDelta: 0.0421
+                 }}/>
+    );
 }
+
+const styles = StyleSheet.create({
+    map: {
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+    },
+});
