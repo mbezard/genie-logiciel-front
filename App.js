7e821ce436c7d47aef58e24c8d927c264f53462f
Charles-Emmanuel <charlesemmanuel.bray@gmail.com>
2021-06-07 11:08:43 +0200

    edit navbar  + add router

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {Link, NativeRouter, Route} from "react-router-native";
 import Home from "./src/components/pages/Home";
 import Profile from "./src/components/pages/Profile";
+import SignIn from './src/components/pages/SignIn';
+import SignOut from './src/components/pages/SignOut';
+import VisitorHome from './src/components/pages/VisitorHome';
 import WanderMap from "./src/components/pages/WanderMap";
 import {padding} from "./src/components/utils/utils";
 
@@ -15,6 +18,9 @@ export default function App() {
                     <Route exact path="/" component={Home}/>
                     <Route path="/map" component={WanderMap}/>
                     <Route path="/profile" component={Profile}/>
+                    <Route path="/visitorHome" component={VisitorHome}/>
+                    <Route path="/signIn" component={SignIn}/>
+                    <Route path="/signOut" component={SignOut}/>
                     </View>
                     {/* Header : */}
                     <View style={styles.navbar}>
@@ -27,7 +33,15 @@ export default function App() {
                         <Link to="/profile" underlayColor="#f0f4f7">
                             <Text style={styles.navItem}>Profile</Text>
                         </Link>
-
+                        <Link to="/visitorHome" underlayColor="#f0f4f7">
+                            <Text style={styles.navItem}>Home</Text>
+                        </Link>
+                        <Link to="/signIn" underlayColor="#f0f4f7">
+                            <Text style={styles.navItem}>SignIn</Text>
+                        </Link>
+                        <Link to="/signUp" underlayColor="#f0f4f7">
+                            <Text style={styles.navItem}>SignUp</Text>
+                        </Link>
                     </View>
 
                 </View>
