152fc95123dc62fbf0f98c1f2d0f2b8f3604163e
LouisDvr <louis.develter@gmail.com>
2021-06-08 19:04:11 +0200

    Centered initial region on the "Point Zéro"

--- src/components/pages/WanderMap.js
+++ src/components/pages/WanderMap.js
@@ -8,10 +8,10 @@ export default function WanderMap() {
                  provider={PROVIDER_GOOGLE}
                  showsUserLocation
                  initialRegion={{
-                     latitude: 48.5112,
-                     longitude: 2.2055,
-                     latitudeDelta: 0.0922,
-                     longitudeDelta: 0.0421
+                     latitude: 48.853562,
+                     longitude: 2.348094,
+                     latitudeDelta: 0.0142,
+                     longitudeDelta: 0.0102
                  }}/>
     );
 }
