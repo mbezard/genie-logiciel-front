a26d970ff5ca4f49b6bb8f30b36a9513b2180c24
LouisDvr <louis.develter@gmail.com>
2021-06-09 14:50:33 +0200

    Extract position as state and deltas as const

--- src/components/pages/WanderMap.js
+++ src/components/pages/WanderMap.js
@@ -1,18 +1,23 @@
-import React from "react";
-import {StyleSheet, Text, View, TouchableHighlight, Dimensions} from "react-native";
+import React, {useState} from "react";
+import {StyleSheet, Dimensions} from "react-native";
 import MapView, {PROVIDER_GOOGLE} from "react-native-maps";
 
 export default function WanderMap() {
+    const LATITUDE_DELTA = 0.0142;
+    const LONGITUDE_DELTA = 0.0102;
+
+    const [position, setPosition] = useState({
+        latitude: 48.853562,
+        longitude: 2.348094,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+    });
+
     return (
         <MapView style={styles.map}
                  provider={PROVIDER_GOOGLE}
                  showsUserLocation
-                 initialRegion={{
-                     latitude: 48.853562,
-                     longitude: 2.348094,
-                     latitudeDelta: 0.0142,
-                     longitudeDelta: 0.0102
-                 }}/>
+                 initialRegion={position}/>
     );
 }
 
