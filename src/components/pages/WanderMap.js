4a5bb4271c49719c8e7056185d55eda6b8e43a5c
LouisDvr <louis.develter@gmail.com>
2021-06-09 16:10:31 +0200

    Add location tracker in foreground to center on user position

--- src/components/pages/WanderMap.js
+++ src/components/pages/WanderMap.js
@@ -1,23 +1,44 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {StyleSheet, Dimensions} from "react-native";
 import MapView, {PROVIDER_GOOGLE} from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function WanderMap() {
     const LATITUDE_DELTA = 0.0142;
     const LONGITUDE_DELTA = 0.0102;
 
-    const [position, setPosition] = useState({
+    const [location, setLocation] = useState({
         latitude: 48.853562,
         longitude: 2.348094,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
     });
+
+    const getLocationAsync = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync()
+            .catch(err => console.log(err));
+        if (status !== 'granted') {
+            throw new Error("Permission to access location was denied");
+        }
+
+        let locationObject = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High})
+            .catch(err => console.log(err));
+        setLocation({latitude: locationObject.coords.latitude, longitude: locationObject.coords.longitude});
+    }
+
+    useEffect(() => {
+        getLocationAsync().catch(err => console.log(err));
+    }, []);
 
     return (
         <MapView style={styles.map}
                  provider={PROVIDER_GOOGLE}
                  showsUserLocation
-                 initialRegion={position}/>
+                 followsUserLocation
+                 region={{
+                     latitude: location.latitude,
+                     longitude: location.longitude,
+                     latitudeDelta: LATITUDE_DELTA,
+                     longitudeDelta: LONGITUDE_DELTA
+                 }}/>
     );
 }
 
