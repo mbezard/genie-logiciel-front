24816d45f3a7bd674de64ca11e4a0b23723db12b
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-06-09 17:03:38 +0200

    npm i + some problems solved

--- App.js
+++ App.js
@@ -1,80 +1,67 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import Home from "./src/components/pages/Home";
 import Profile from "./src/components/pages/Profile";
-import SignIn from './src/components/pages/SignIn';
-import SignUp from './src/components/pages/SignUp';
-import VisitorHome from './src/components/pages/VisitorHome';
 import WanderMap from "./src/components/pages/WanderMap";
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator, tabBarBadge } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Ionicons} from '@expo/vector-icons';
 import {Provider} from "react-redux";
 import store from "./src/components/utils/store/store";
 
 export default function App() {
     const Tab = createBottomTabNavigator();
-    return (
-            <Provider store={store}>
-                <SafeAreaView style={styles.mainContainer}>
-
-            <NavigationContainer style={styles.container}>
-                <Tab.Navigator style={styles.mainView}
-                        screenOptions={({ route }) => ({
-                            tabBarIcon: ({ focused, color, size }) => {
-                            if (route.name === 'Home') {
-                                return (
-                                <Ionicons
-                                    name={
-                                    focused
-                                        ? 'home'
-                                        : 'home-outline'
-                                    }
-                                    size={size}
-                                    color={color}
-                                />
-                                );
-                            } else if (route.name === 'Map') {
-                                return (
-                                <Ionicons
-                                    name={focused ? 'map' : 'map-outline'}
-                                    size={size}
-                                    color={color}
-                                />
-                                );
-                            } else if (route.name === 'Profile') {
-                                return (
-                                <Ionicons
-                                    name={focused ? 'person-circle' : 'person-circle-outline'}
-                                    size={size}
-                                    color={color}
-                                />
-                                );
-                            } else if (route.name === 'Proposer lieu') {
-                                return (
-                                <Ionicons
-                                    name={focused ? 'add-circle' : 'add-circle-outline'}
-                                    size={size}
-                                    color={color}
-                                />
-                                );
-                            }
-                            },
-                        })}
-                        tabBarOptions={{
-                            activeTintColor: 'tomato',
-                            inactiveTintColor: 'gray',
-                        }}>
-                    <Tab.Screen name="Home" component={Home}/>
-                    <Tab.Screen name="Map" component={WanderMap}/>
-                    <Tab.Screen name="Profile" component={Profile}/>
-                    <Tab.Screen name="Proposer lieu" component = {Profile}/>
-                </Tab.Navigator>
-            </NavigationContainer>
-                </SafeAreaView>
-            </Provider>
-        </>
-    );
+    return (<>
+        <Provider store={store}>
+            <SafeAreaView style={styles.mainContainer}>
+                <NavigationContainer style={styles.container}>
+                    <Tab.Navigator style={styles.mainView}
+                                   screenOptions={({route}) => ({
+                                       tabBarIcon: ({focused, color, size}) => {
+                                           if (route.name === 'Home') {
+                                               return (
+                                                   <Ionicons
+                                                       name={focused ? 'home' : 'home-outline'}
+                                                       size={size}
+                                                       color={color}/>
+                                               );
+                                           } else if (route.name === 'Map') {
+                                               return (
+                                                   <Ionicons
+                                                       name={focused ? 'map' : 'map-outline'}
+                                                       size={size}
+                                                       color={color}/>
+                                               );
+                                           } else if (route.name === 'Profile') {
+                                               return (
+                                                   <Ionicons
+                                                       name={focused ? 'person-circle' : 'person-circle-outline'}
+                                                       size={size}
+                                                       color={color}/>
+                                               );
+                                           } else if (route.name === 'Proposer lieu') {
+                                               return (
+                                                   <Ionicons
+                                                       name={focused ? 'add-circle' : 'add-circle-outline'}
+                                                       size={size}
+                                                       color={color}/>
+                                               );
+                                           }
+                                       },
+                                   })}
+                                   tabBarOptions={{
+                                       activeTintColor: 'tomato',
+                                       inactiveTintColor: 'gray',
+                                   }}>
+                        <Tab.Screen name="Home" component={Home}/>
+                        <Tab.Screen name="Map" component={WanderMap}/>
+                        <Tab.Screen name="Profile" component={Profile}/>
+                        <Tab.Screen name="Proposer lieu" component={Profile}/>
+                    </Tab.Navigator>
+                </NavigationContainer>
+            </SafeAreaView>
+        </Provider>
+    </>);
 }
 
 const styles = StyleSheet.create({
@@ -89,15 +76,5 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
-    },
-    navbar:{
-        ...padding(4),
-        flexDirection: "row",
-        backgroundColor: "orange",
-        borderWidth: 1,
-        borderColor: "grey",
-    },
-    navItem : {
-        ...padding(4, 8, 4, 8)
     }
 });
