61f21cb28b40628768306e77863883cd54fa4cd8
Charles-Emmanuel <charlesemmanuel.bray@gmail.com>
2021-06-13 21:53:16 +0200

    add and edit inputs + better design

--- src/components/utils/requests/place.js
+++ src/components/utils/requests/place.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 import {ADD_PLACE_URL} from "../Url";
 
-export const addNewPlace = async (nom, adresse, inputTags ) => {
+export const addNewPlace = async (name, address, description, tags, location, authorMail ) => {
     const data = new FormData();
-    data.append("nom", nom);
-    data.append("adresse", adresse);
-    data.append("inputTags", inputTags);
+    data.append("name", name);
+    data.append("address", address);
+    data.append("tagsAsString", JSON.stringify(tags))
+    data.append("description", description);
+    data.append("latitude", location.latitude);
+    data.append("longitude", location.longitude);
+    data.append("authorMail", authorMail);
     let error = false;
     const response = (await axios.post(ADD_PLACE_URL, data).catch(reason => error = reason.response));
     return {data: response.data, error: error}
